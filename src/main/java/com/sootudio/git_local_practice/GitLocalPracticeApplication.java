41ac0af48a876c38f0be7ebb95417b629bba6d9f
Sooyoung <kswim8914@gmail.com>
2025-08-31 23:11:10 +0900

    init: new project intialization

---
+++ src/main/java/com/sootudio/git_local_practice/GitLocalPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sootudio.git_local_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLocalPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLocalPracticeApplication.class, args);
+	}
+
+}
